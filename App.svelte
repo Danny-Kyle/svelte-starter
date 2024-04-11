96400e825a1b0116cef2d48d72a9dce27495ea3b
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-11 16:40:45 +0100

    first commit

---
+++ App.svelte
@@ -0,0 +1,41 @@
+<!--First lesson: First Component -->
+<script>
+	const name = 'Svelte';
+</script>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<!--Second lesson: Dynamic Attributes-->
+<script>
+	let src = '/image.gif';
+	const name = 'Rick Astley';
+</script>
+
+<img {src} alt="{name} dances." />
+
+<!--Third Lesson: Styling-->
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<!-- Fourth Lesson: Nested Components -->
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
+<p>This is a paragraph.</p>
+<Nested />
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
