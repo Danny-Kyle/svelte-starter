4186cecf5dd7f0af9133fb6715e280d797b7d5eb
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-11 17:08:01 +0100

    continued lessons

--- App.svelte
+++ App.svelte
@@ -39,3 +39,83 @@
 		font-size: 2em;
 	}
 </style>
+
+<!--Fifth Lesson: HTML Tags-->
+<script>
+	let string = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
+<p>{@html string}</p>
+
+
+<!--Sixth Lesson: Reactivity-->
+<script>
+    let count = 0;
+
+    function increment() {
+        //event handler goes in here
+        count += 1;
+    }
+</script>
+
+<button on:click={increment}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+</button>
+
+<!--Seventh Lesson: Declarations-->
+<script>
+    let count = 0;
+    $: doubled = count * 2;
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<button on:click={increment}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} double is {doubled}</p>
+
+<!--Eighth Lesson: Statements-->
+<script>
+    let count = 0;
+
+    $: if(count > 10){
+        alert('count is dangerously high');
+        count = 0;
+    }
+
+    function handleClick(){
+        count += 1;
+    }
+</script>
+
+<button on:click={handleClick}>
+    Clicked {count}
+    {count === 1 ? 'time' : "times"}
+</button>
+
+<!--Ninth Lesson: Updating Arrays and objects-->
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+        //method one
+		// numbers.push(numbers.length + 1);
+        // numbers = numbers;
+
+        //method two
+        numbers  = [...numbers, numbers.length + 1];
+	}
+
+	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
